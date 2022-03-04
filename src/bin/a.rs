b38d1f3a6dd541c7219d779eb82cd07336f068ca
Kenbayashi <kenbayashi.gaming.00@gmail.com>
2022-03-04 16:17:27 +0900

    a

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -1,3 +1,15 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    unimplemented!();
+    input! {
+        s: Chars,
+    }
+
+    let (a, b) = (String::from(s[0]), String::from(s[2]));
+
+    let a = a.parse::<i64>().ok().unwrap();
+    let b = b.parse::<i64>().ok().unwrap();
+
+    println!("{}", a * b);
 }
