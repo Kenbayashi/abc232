d02d5a7d09d645172144fc2c879e9fd00ee10e8f
Kenbayashi <kenbayashi.gaming.00@gmail.com>
2022-03-04 16:32:54 +0900

    b

--- src/bin/b.rs
+++ src/bin/b.rs
@@ -1,3 +1,16 @@
+use proconio::input;
+use proconio::marker::Bytes;
+
 fn main() {
-    unimplemented!();
+    input! {
+        s: Bytes,
+        t: Bytes,
+    }
+
+    let ans = s.into_iter()
+               .zip(t.into_iter())
+               .map(|(a, b)| (a as i32 - b as i32) % 26)
+               .collect::<Vec<i32>>();
+
+    println!("{}", if ans.iter().min() == ans.iter().max() {"Yes"} else {"No"});
 }
